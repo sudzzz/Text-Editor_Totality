487a8f062c45edd5fe911f273ed64ac58dcdd6fc
sudzzz <sudhirdaga1998@gmail.com>
2020-01-25 11:27:16 +0530

    EditText

---
+++ app/src/main/java/com/daga/edittext/MainActivity.java
@@ -0,0 +1,34 @@
+package com.daga.edittext;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        final AmazingAutofitEditText autofitEditText = findViewById(R.id.textView);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view,
+                        Objects.requireNonNull(autofitEditText.getText()).toString(),
+                        Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+}
